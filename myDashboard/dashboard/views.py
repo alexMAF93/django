d0b599e485fdab15748e5922175b73adb7c3b1b5
alexMAF93 <mitroi.alex93@gmail.com>
2020-09-15 06:16:30 +0300

    I gave up :(

--- myDashboard/dashboard/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-
-def index(request):
-    return_dict = {'message': ['Hello,', 'World!']}
-
-    return render(request, 'dashboard/index.html', return_dict)
