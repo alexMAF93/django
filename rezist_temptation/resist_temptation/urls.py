a35e735e177108911a8f8906e630f35c6f35cc08
Alex Mitroi <amitroi@localhost.localdomain>
2021-09-08 09:26:41 -0700

    urls updated

--- rezist_temptation/resist_temptation/urls.py
+++ rezist_temptation/resist_temptation/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/temptations', views.temptations, name='temptations'),
-    path('/good_habits', views.good_habits, name='good_habits'),
-    path('/temptations/statistics', views.temptations_statistics, name='temptations_statistics'),
-    path('/good_habits/statistics', views.good_habits_statistics, name='good_habits_statistics'),
+    path('temptations', views.temptations, name='temptations'),
+    path('good_habits', views.good_habits, name='good_habits'),
+    path('temptations/statistics', views.temptations_statistics, name='temptations_statistics'),
+    path('good_habits/statistics', views.good_habits_statistics, name='good_habits_statistics'),
     ]
